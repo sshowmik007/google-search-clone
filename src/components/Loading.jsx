268c6660e8bc622e95e3d6b63db2cb9cb7743355
sshowmik <sadman.showmik@g.bracu.ac.bd>
2022-05-15 22:14:20 +0600

    15 MAY

---
+++ src/components/Loading.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Loader from 'react-loader-spinner';
+// import { BoltLoader } from "react-awesome-loaders";
+
+export const Loading = () => (
+  <div className="flex justify-center items-center ">
+    {/* <Loader type="Puff" color="#00BFFF" height={550} width={80} /> */}
+    {/* <BoltLoader
+        className={"loaderbolt"}
+        boltColor={"#6366F1"}
+        backgroundBlurColor={"#E0E7FF"}
+
+      /> */}
+      <h1>LOADING</h1>
+  </div>
+);
---
+++ src/contexts/StateContextProvider.jsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const StateContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getResults = async (url) => {
+    setLoading(true);
+
+    const res = await fetch(`${baseUrl}${url}`, {
+      method: 'GET',
+      headers: {
+    'X-User-Agent': 'desktop',
+    'X-Proxy-Location': 'US',
+    'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+    'X-RapidAPI-Key': 'b99f688d5fmsh596210b2193a3c1p1d3c63jsn773ff74df2a9'
+      },
+    });
+
+    const data = await res.json();
+
+    setResults(data);
+    setLoading(false);
+  };
+
+  return (
+    <StateContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
